2793c2ad2d469904c21f0b159883bdbfcc3f3872
kaityhallman <kaityhallman@gmail.com>
2019-10-23 10:50:37 -0400

    Updates

---
+++ precache-manifest.381b0a7777dfcf253cdec6d069275561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d1619933fabd6a6c08c826e3bd6d2f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33c0c9d23e0d875842c2",
+    "url": "/static/css/main.782f3c56.chunk.css"
+  },
+  {
+    "revision": "0634c0859a19f8d64ecc",
+    "url": "/static/js/2.704f5e02.chunk.js"
+  },
+  {
+    "revision": "33c0c9d23e0d875842c2",
+    "url": "/static/js/main.35078d75.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aad0077ac6f7c5c316e9a3d0b442cc03",
+    "url": "/static/media/kaity.aad0077a.jpg"
+  }
+]);
